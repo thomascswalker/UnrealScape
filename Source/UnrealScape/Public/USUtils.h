766d8b6fba46888ca486f132d16201239f28a460
thomascswalker <thomascswalker@gmail.com>
2023-02-01 09:02:45 -0500

    minor update to utils

--- Source/UnrealScape/Public/USUtils.h
+++ Source/UnrealScape/Public/USUtils.h
@@ -8,6 +8,10 @@
     GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::White, \
                                      FString(__FUNCTION__) + "(" + FString::FromInt(__LINE__) + ") " + Message);
 
+#define SUCCESS(Message)                                      \
+    GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Green, \
+                                     FString(__FUNCTION__) + "(" + FString::FromInt(__LINE__) + ") " + Message);
+
 #define WARNING(Message)                                       \
     GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, \
                                      FString(__FUNCTION__) + "(" + FString::FromInt(__LINE__) + ") " + Message);
